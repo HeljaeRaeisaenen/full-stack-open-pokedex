f856892c6d0de3a0a901576ad098f48c1d99ab88
HeljaeRaeisaenen <94612974+HeljaeRaeisaenen@users.noreply.github.com>
2025-04-01 11:38:35 +0300

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   },
   devServer: {
     historyApiFallback: true,
-    allowedHosts: ["https://full-stack-open-pokedex-1kvl.onrender.com/"],
+    allowedHosts: "all",
   },
   plugins: [
     new HtmlWebPackPlugin({
